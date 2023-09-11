3a853ffb8ca82395bab19fb5424353f6c716df32
sagarsinha162 <79259811+sagarsinha162@users.noreply.github.com>
2023-09-11 15:27:44 +0530

    style.js

---
+++ style.js
@@ -0,0 +1,14 @@
+
+
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.addEventListener('click', () => {
+  menu.classList.toggle('bx-x');
+  navbar.classList.toggle('active');
+});
+
+window.addEventListener('scroll', () => {
+  menu.classList.remove('bx-x');
+  navbar.classList.remove('active');
+});
